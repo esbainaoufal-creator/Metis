62642918da0dd1aa5e4d19a83f3d9f1268797eed
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-23 14:47:58 +0100

    SQL Modifications : projects table created

--- Metis.session.sql
+++ Metis.session.sql
@@ -4,3 +4,17 @@ CREATE TABLE members (
     email VARCHAR(150) NOT NULL UNIQUE,
     created_at DATETIME NOT NULL
 ) ENGINE=InnoDB;
+
+--@block
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    created_at DATETIME NOT NULL,
+    member_id INT NOT NULL,
+    type ENUM('short', 'long') NOT NULL,
+    CONSTRAINT fk_project_member
+        FOREIGN KEY (member_id)
+        REFERENCES members(id)
+        ON DELETE RESTRICT
+) ENGINE=InnoDB;
