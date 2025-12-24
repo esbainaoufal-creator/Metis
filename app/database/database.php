8a4a7e121099c7be514bbaa929e5c07e7999bf36
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-24 10:03:59 +0100

    Add Config class and reuse it in Database PDO singleton

---
+++ app/database/database.php
@@ -0,0 +1,42 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $config = Config::database();
+
+        try {
+            $this->connection = new PDO(
+                "mysql:host={$config['host']};dbname={$config['dbName']};charset=utf8",
+                $config['username'],
+                $config['password']
+            );
+
+            $this->connection->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+        } catch (PDOException $e) {
+            die("Database error: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
