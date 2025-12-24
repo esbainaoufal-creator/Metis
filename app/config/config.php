7da4ef09876233451bc9d1839aeb8f8a20ff8b6e
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-24 10:00:53 +0100

    config file well created

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config
+{
+    public static function database()
+    {
+        return [
+            'host' => 'localhost',
+            'dbName' => 'project_manager',
+            'username' => 'root',
+            'password' => ''
+        ];
+    }
+}
